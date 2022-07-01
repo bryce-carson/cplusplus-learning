eadbe8a9236fc4f515a4e5160142fc0731b567d4
bryce-carson <bcars268@mtroyal.ca>
2022-06-30 20:17:59 -0600

    Comment on the Git workflow in `main.cpp`
    Explain the Git workflow, very briefly and not completely, in a comment in the `main.cpp` file.

--- main.cpp
+++ main.cpp
@@ -4,3 +4,10 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+/*
+ * This is a test of the Git workflow, using new branches based off an initial 'empty' commit, containing only the base
+ * project files and the standard "Hello, World!" boilerplate in a new CLion C++ project.
+ *
+ * This comment will should be removed in a subsequent commit, and will not be present in any other branches.
+ */
